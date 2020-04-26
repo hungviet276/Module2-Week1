87a754b33e7a4f67a0dd1b7876975e640a165655
Hung Viet <you@example.com>
2020-04-26 10:36:43 +0700

    add new file

---
+++ TinhLaiNganHang/src/TinhLaiNganHang.java
@@ -0,0 +1,20 @@
+public class TinhLaiNganHang {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interset_rate = 1.0;
+
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.println("Money: ");
+        money = input.nextDouble();
+        System.out.println("Time (month): ");
+        month = input.nextInt();
+        System.out.println("Interest rate: ");
+        interset_rate = input.nextDouble();
+
+        double total_interset = 0;
+            total_interset = money * (interset_rate / 100) / 12 * month;
+
+        System.out.println("Total interset: " + total_interset);
+    }
+}
